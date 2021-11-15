690e827f0c9c4f8b6e1a36849e3f84db3b47b97c
f0r3iddenn3tworks <rene.kretzinger@f0r3idd3n-n3tw0rk2.ch>
2021-11-15 21:08:41 +0100

    directory 01

--- 04_Go-Code-Testcode/Program01.go
+++ 04_Go-Code-Testcode/Program01.go
@@ -10,3 +10,15 @@ func main() {
 	//test the code with go run Program01.go
 	fmt.Println("Hello, Black Hat Gophers")
 }
+
+//go build Program01.go
+//-> create an executable ./Program01
+
+// reduce the binary size: go build -ldflags"-w -s"
+//without debugging and symbol table
+
+//GOOS Operating System
+//GOARCH Architecture
+//GOOS="linux" GOARCH="amd64" go build Program01.go
+
+//go doc fmt.Println -> get information
